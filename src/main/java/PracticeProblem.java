f055b6680bc2688d859ce158d0723122374a30ec
ALEX-L1ANG <343293684@tdsb.ca>
2026-02-17 16:41:03 +0000

    update1

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,12 @@
+/*
+Lesson: Input
+Author: Alex Liang
+Date Created : Feb 17, 2026
+Date Last Modified: Feb 17, 2026
+*/
+
+import java.util.Scanner;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -10,18 +19,44 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
+	String name;
+	Scanner input = new Scanner(System.in);
+	System.out.print("What is your name: ");
+	name = input.nextLine();
+	System.out.print(name);
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+	int num;
+	System.out.print("Input a whole number: ");
+	num = input.nextInt();
+	input.nextLine();
+	System.out.println(num);
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+	double radius;
+	System.out.print("Input a radius: ");
+	radius = input.nextDouble();
+	input.nextLine();
+	System.out.print(3.14 * radius * 2);
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+	string firstName;
+	string lastName;
+	int age;
+
+	System.out.print("Input a first name: ");
+	firstName = input.nextLine();
+	lastName = input.nextLine();
+	age = input.nextInt();
+	input.nextLine();
+	System.out.print(firstName + " " + lastName + "-" );
+	
 	}
 
 	public static void q5() {
